d27943d1c44dab12830335389f623911389c39a8
Josephine Gavignet <josephinegavignet@gmail.com>
2017-08-12 22:13:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ function total() {
   let itemPrices = []
   let totalPrice = 0
   for (var key in cart) {
-    itemPrices.push(Object.keys(itemInfo)[1])
+    itemPrices.push(Object.keys(key)[1])
   }
   for (let i = 0; i < itemPrices.length; i++) {
     totalPrice += parseInt(itemPrices[i])
