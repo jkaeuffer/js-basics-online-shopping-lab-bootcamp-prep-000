69ba3532d7d2212bf9a9a3f09bb3a8da73940dd8
Josephine Gavignet <josephinegavignet@gmail.com>
2017-08-12 22:16:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ function viewCart() {
 }
 addToCart('banana')
 addToCart('apple')
-console.log(cart[0][1])
+console.log(cart[0])
 /*
 The total() function accepts no arguments, iterates through the cart array,
 and returns the current total value of the items in the cart.*/
